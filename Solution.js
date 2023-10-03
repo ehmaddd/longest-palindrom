a04e37bb63abf3802ba774df954bdfe08c7e226a
ehmaddd <ehmaddd@gmail.com>
2023-10-03 21:04:14 +0500

    Find all combinations

--- Solution.js
+++ Solution.js
@@ -1,7 +1,18 @@
 var longestPalindrome = function(s) {
+  const pal = [];
   for(let i=0; i<s.length; i++){
-    console.log(s[i]);
+    for(let j=i; j<s.length; j++){
+      if(s[i]==s[j]){
+        let str = '';
+        for(let k=i; k<=j; k++){
+          str += s[k];
+        }
+        pal.push(str);
+        str = '';
+      }
+    }
   }
+  console.log(pal);
 };
 
 longestPalindrome("babad");
